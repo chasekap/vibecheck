0e448f7440c030649608c64c939b065ee219e4e6
chasekap <chasekap@users.noreply.github.com>
2020-05-03 19:28:18 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+//shitty ass comment
 export default function App() {
   return (
     <View style={styles.container}>
