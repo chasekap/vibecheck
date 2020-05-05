fa6c94c0e099d2184c2ecdb513d5f5002897127b
corddry <jackcorddry@ucla.edu>
2020-05-05 16:26:48 -0700

    Correct modularization output to match text to score

--- search.py
+++ search.py
@@ -68,7 +68,6 @@ def search_twitter(keyword):
   for tweet in public_tweets:
     tweety = tweet.text
     twitter_comments.append(tweety)
-    print(tweety)
     
 
 #Test because ppl love to tweet about him
@@ -77,7 +76,7 @@ def analyze_text(texts):
     num_datum += len(texts)
     for text in texts: 
         compound_sentiment = analyser.polarity_scores(text).get('compound')
-        print("Compund sentiment: ", compound_sentiment, " - ", 
+        print("Tweet: ", text, "\nCompund sentiment: ", compound_sentiment, " - ",
                 interpret_compound_score(compound_sentiment), "\n")
         sentiment_sum += compound_sentiment
         
