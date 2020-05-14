14c3bc0243f7cad428bcda02c0af523e590bf9b6
AristotleH <53080395+AristotleH@users.noreply.github.com>
2020-05-13 22:45:27 -0700

    Reformatted README and refactored index.js

--- vibecheck/src/index.js
+++ vibecheck/src/index.js
@@ -1,32 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
-import './index.css';
-import { Input } from 'antd';
-import { Row, Col} from 'antd';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const { Search } = Input; 
+import "./index.css";
 
+import "antd/dist/antd.css";
 
-function getData(search){
-  fetch(`/search/${search}`).then(res => res.json()).then(data => {
-    console.log(data)
-  });
+import { Input } from "antd";
+import { Row, Col } from "antd";
+import { Layout, Menu, Breadcrumb } from "antd";
+
+const { Header, Content, Footer } = Layout;
+const { Search } = Input;
+
+class ParentLayout extends React.Component {
+    render() {
+        return (
+            <Layout style={{ height: "100vh", overflow: "auto" }}>
+                <SearchFormContent />
+            </Layout>
+        );
+    }
 }
 
-const MyRow = () => (
-  <Row type="flex" align="middle" style={{padding: '30% 0% 0% 0%'}}>
-    <Col md={10}>
-    </Col>
-    <Col md={5}><div>
-    <Search
-placeholder="input search text"
-onSearch={value => getData(value)}
+class SearchFormContent extends React.Component {
+    render() {
+        return (
+            <Content>
+                <div className="site-layout-content">
+                    <Row
+                        type="flex"
+                        justify="center"
+                        align="middle"
+                        style={{ height: "100%" }}
+                    >
+                        <Col type="flex" justify="center" align="middle">
+                            <Search
+                                placeholder="what's on your mind?"
+                                onSearch={(value) => getData(value)}
+                                enterButton
+                            />
+                        </Col>
+                    </Row>
+                </div>
+            </Content>
+        );
+    }
+}
 
-/></div></Col>
-<Col md={9}>
-    </Col>
-  </Row>
+function getData(search) {
+    fetch(`/search/${search}`)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+        });
+}
+
+ReactDOM.render(
+    <ParentLayout />, 
+    document.getElementById("root")
 );
-const rowy =  <MyRow/>
-ReactDOM.render(rowy, document.getElementById('root'));
