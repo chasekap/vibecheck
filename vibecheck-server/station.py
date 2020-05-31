3f9155f9f72f628abcbadfedd55f8b484d664db2
AristotleH <53080395+AristotleH@users.noreply.github.com>
2020-05-31 01:22:07 -0700

    Inital MySQL implementation and testing

--- vibecheck-server/station.py
+++ vibecheck-server/station.py
@@ -1,26 +1,44 @@
 import search as s
 from flask import Flask
 from flask import request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost:3306/vibecheck_db'
+db = SQLAlchemy(app)
+
 
 @app.route('/search/<search>')
 def search_request(search):
 
+    db.create_all()
     #tweets = s.search_twitter(search)
     urls = s.search_google(search)
-    #urls = ["https://www.reddit.com/r/SFGiants/","https://www.reddit.com/r/Politics/"] <-- good way to test if out of searches
+    # urls = ["https://www.reddit.com/r/SFGiants/","https://www.reddit.com/r/Politics/"] <-- good way to test if out of searches
     coms = s.search_reddit(urls)
-    avg_sentiment, sample = s.analyze_text(coms,search)
-    
+    avg_sentiment, sample = s.analyze_text(coms, search)
+
     word_count = s.word_count(coms)
     output_dict = {
-        "urls": urls, 
+        "urls": urls,
         "avg_sentiment": avg_sentiment,
         "word_count": word_count,
-        "comments" : len(coms),
-        "sample" : sample
-        
+        "comments": len(coms),
+        "sample": sample
+
     }
+
+    search_db_entry = UserSearch(search=search)
+
+    db.session.add(search_db_entry)
+    db.session.commit()
+
     return output_dict
-    
+
+
+class UserSearch(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    search = db.Column(db.String(280), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Search %r>' % self.search
